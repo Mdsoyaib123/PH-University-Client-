b44f31c816d44c361f7694bb2eaeb46ee0950abd
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2025-03-17 11:40:18 +0600

    update

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,33 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
 
 const Login = () => {
+  const { register, handleSubmit } = useForm();
+  const [login, { data, error }] = useLoginMutation();
+  console.log(data);
+  console.log(error);
+  
+  const onsubmit = (data) => {
+    const userInfo = {
+      id: data.id,
+      Password: data.password,
+    };
+    login(userInfo);
+  };
   return (
-    <div>
-      login
-    </div>
-  )
-}
+    <form onSubmit={handleSubmit(onsubmit)}>
+      <div>
+        <label>ID :</label>
+        <input type="text" id="id" {...register("id")} />
+      </div>
+      <div>
+        <label>PASSWORD :</label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+      <Button htmlType="submit">Submit</Button>
+    </form>
+  );
+};
 
-export default Login
+export default Login;
